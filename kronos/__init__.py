447836d18d2a3f579a45eb80338e0338103ccfa6
Jody Meredi <jodymeredi@users.noreply.github.com>
2026-04-12 22:27:51 +0800

    add BatchPredictor to __all__ and bump version to 0.1.0-personal

--- kronos/__init__.py
+++ kronos/__init__.py
@@ -15,19 +15,25 @@ Most of my work is in the examples/ directory.
 
 Personal note: added preprocess_ohlcv and scale_features to __all__
 so they're easier to import directly when doing quick experiments.
+
+Fork changes:
+    - Bumped __version__ to reflect local modifications
+    - Added BatchPredictor to __all__ for easier access in notebooks
 """
 
 from kronos.model import KronosModel
 from kronos.data import DataLoader, preprocess_ohlcv
 from kronos.utils import normalize, denormalize, scale_features
+from kronos.batch import BatchPredictor  # useful for running overnight batch jobs on crypto data
 
-__version__ = "0.1.0"
+__version__ = "0.1.0-personal"
 __author__ = "Kronos Contributors"
 __license__ = "MIT"
 
 __all__ = [
     "KronosModel",
     "DataLoader",
+    "BatchPredictor",
     "preprocess_ohlcv",
     "normalize",
     "denormalize",
